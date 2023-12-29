9cbabb8f2ea7b173a599a841cbbb2b9f2c3a59e1
brstrutt <ben.strutt@hotmail.co.uk>
2023-12-29 10:28:33 +0000

    move page description to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,14 @@
 use yew::prelude::*;
 
 mod flood_monitoring_api;
+mod page;
 mod header;
-mod footer;
 mod river_level_display;
-
-#[hook]
-fn use_river_level(monitoring_station_id: i32) -> UseStateHandle<Option<flood_monitoring_api::LatestReading>> {
-    let river_level = use_state(|| Option::<flood_monitoring_api::LatestReading>::None);
-    {
-        let river_level = river_level.clone();
-        use_effect_with((), move |_| {
-            wasm_bindgen_futures::spawn_local(async move {
-                let result = flood_monitoring_api::get_river_levels(monitoring_station_id).await;
-    
-                if result.is_ok() {
-                    match result.unwrap().items.first() {
-                        Some(first_data) => river_level.set(Some(first_data.latestReading)),
-                        _ => (),
-                    }
-                }
-                else {
-                    log::error!("Failed to retreive river level data: {:?}", result);
-                }
-            });
-            || ()
-        });
-    }
-    river_level
-}
+mod footer;
 
 #[function_component(App)]
 fn app() -> Html {
-    let barbourne_river_level = use_river_level(2642);
-    html! {
-        <div class="content_overlay">
-            <header::Header/>
-            <main>
-                <river_level_display::RiverLevelDisplay 
-                    barbourne_last_reading={*barbourne_river_level}
-                />
-            </main>
-            <footer::Footer/>
-        </div>
-    }
+    html! { <page::Page/> }
 }
 
 fn main() {
---
+++ src/page.rs
@@ -0,0 +1,46 @@
+use yew::prelude::*;
+use super::flood_monitoring_api;
+use super::river_level_display::RiverLevelDisplay;
+use super::header::Header;
+use super::footer::Footer;
+
+#[hook]
+fn use_river_level(monitoring_station_id: i32) -> UseStateHandle<Option<flood_monitoring_api::LatestReading>> {
+    let river_level = use_state(|| Option::<flood_monitoring_api::LatestReading>::None);
+    {
+        let river_level = river_level.clone();
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let result = flood_monitoring_api::get_river_levels(monitoring_station_id).await;
+    
+                if result.is_ok() {
+                    match result.unwrap().items.first() {
+                        Some(first_data) => river_level.set(Some(first_data.latestReading)),
+                        _ => (),
+                    }
+                }
+                else {
+                    log::error!("Failed to retreive river level data: {:?}", result);
+                }
+            });
+            || ()
+        });
+    }
+    river_level
+}
+
+#[function_component(Page)]
+pub fn page() -> Html {
+    let barbourne_river_level = use_river_level(2642);
+    html! {
+        <div class="content_overlay">
+            <Header/>
+            <main>
+                <RiverLevelDisplay 
+                    barbourne_last_reading={*barbourne_river_level}
+                />
+            </main>
+            <Footer/>
+        </div>
+    }
+}
