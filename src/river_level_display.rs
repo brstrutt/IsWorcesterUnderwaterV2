1fd39dc94b651d2cd72e682808b3322fa7709cf6
brstrutt <ben.strutt@hotmail.co.uk>
2024-10-19 12:12:42 +0000

    refactor river level display for readability

--- src/river_level_display.rs
+++ src/river_level_display.rs
@@ -1,6 +1,8 @@
 mod css_keyframe_animation;
 mod wave_animation;
 
+use std::string;
+
 use yew::prelude::*;
 
 use crate::flood_monitoring_api::LatestReading;
@@ -31,38 +33,66 @@ pub fn river_level_display_background(BackgroundProps {barbourne_last_reading, d
                 class="full_canvas"
                 style={wave_animation.to_string()}
             >
-                <div 
-                    class="full_canvas background_water"
-                    color="aqua"
+                <Waves/>
+                <SolubleText
+                    shown_above_water="No"
+                    shown_below_water="Yes"
                 />
-                <div 
-                    class="full_canvas background_water_2"
-                    color="aqua"
-                />
-                <div 
-                    class="full_canvas clipping_water ShowBelowWaterLevel"
-                    color="blue"
-                />
-                <div 
-                    class="full_canvas result_wrapper ShowAboveWaterLevel"
+            </div>
+        </>
+    }
+}
+
+#[function_component(Waves)]
+fn waves() -> Html {
+    html! {
+        <>
+            <div
+                class="full_canvas background_water"
+                color="aqua"
+            />
+            <div 
+                class="full_canvas background_water_2"
+                color="aqua"
+            />
+            <div 
+                class="full_canvas clipping_water ShowBelowWaterLevel"
+                color="blue"
+            />
+        </>
+    }
+}
+
+
+#[derive(Properties, PartialEq)]
+pub struct SolubleTextProps {
+    pub shown_above_water: String,
+    pub shown_below_water: String
+}
+
+#[function_component(SolubleText)]
+fn soluble_text(SolubleTextProps {shown_above_water, shown_below_water}: &SolubleTextProps) -> Html {
+    html! {
+        <>
+            <div
+                class="full_canvas result_wrapper ShowAboveWaterLevel"
+            >
+                <h1
+                    class="result_content"
+                    style="color: black;"
                 >
-                    <h1
-                        class="result_content"
-                        style="color: black;"
-                    >
-                        {"No"}
-                    </h1>
-                </div>
-                <div
-                    class="full_canvas result_wrapper ShowBelowWaterLevel"
+                    {shown_above_water}
+                </h1>
+            </div>
+            <div
+                class="full_canvas result_wrapper ShowBelowWaterLevel"
+            >
+                <h1
+                    class="result_content" 
+                    style="color: white;"
                 >
-                    <h1
-                        class="result_content" 
-                        style="color: white;"
-                    >
-                        {"Yes"}
-                    </h1>
-                </div>
+                    {shown_below_water}
+                </h1>
             </div>
         </>
     }
