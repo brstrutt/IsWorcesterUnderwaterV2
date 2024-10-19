bc27680a61f2965b57ce4950b7acf0c26538c09f
brstrutt <ben.strutt@hotmail.co.uk>
2024-10-19 16:35:00 +0000

    refactor height_to_screenspace and add tests

--- src/river_level_display.rs
+++ src/river_level_display.rs
@@ -18,13 +18,15 @@ pub struct BackgroundProps {
 
 #[function_component(Background)]
 pub fn river_level_display_background(BackgroundProps {barbourne_last_reading, diglis_last_reading}: &BackgroundProps) -> Html {
+    let barbourne_range = height_to_screenspace::HeightRange{normal_level: 2.0, risky_level: 4.0};
+    let diglis_range = height_to_screenspace::HeightRange{normal_level: 1.5, risky_level: 3.3};
 
     let wave_animation = use_memo(
         (*barbourne_last_reading, *diglis_last_reading), 
         |(barbourne_reading, diglis_reading)| -> WaveAnimation {
-            let barbourne_flood_percentage = height_to_screenspace::get_flood_percentage(barbourne_reading.value as f64, 2.0, 4.0);
-            let diglis_flood_percentage = height_to_screenspace::get_flood_percentage(diglis_reading.value as f64, 1.5, 3.3);
-            WaveAnimation::new(1.0 - barbourne_flood_percentage, 1.0 - diglis_flood_percentage)
+            let barbourne_flood_percentage = height_to_screenspace::get_flood_percentage(barbourne_reading.value as f64, barbourne_range);
+            let diglis_flood_percentage = height_to_screenspace::get_flood_percentage(diglis_reading.value as f64, diglis_range);
+            WaveAnimation::new(1.0 - barbourne_flood_percentage.min(1.0).max(0.0), 1.0 - diglis_flood_percentage.min(1.0).max(0.0))
         }
     );
 
--- src/river_level_display/height_to_screenspace.rs
+++ src/river_level_display/height_to_screenspace.rs
@@ -1,11 +1,92 @@
 
+#[derive(PartialEq, Clone, Copy)]
+pub struct HeightRange {
+    pub normal_level: f64,
+    pub risky_level: f64
+}
 
-pub fn get_flood_percentage(current_level: f64, normal_level: f64, risky_level: f64) -> f64 {
-	let quarter = risky_level - normal_level; // Calculate what 25% of the range is
-	let base = normal_level - quarter; // Calculate what value 0% would be, aka the base of the range
+// Converts the specified river level into a screen height percentage
+// HeightRange.normal_level will sit 25% above the bottom of the screen
+// HeightRange.risky_level will sit 50% above the bottom of the screen
+pub fn get_flood_percentage(current_level: f64, range: HeightRange) -> f64 {
+	let quarter = range.risky_level - range.normal_level; // Calculate what 25% of the range is
+	let base = range.normal_level - quarter; // Calculate what value 0% would be, aka the base of the range
 
 	let scaled_level = current_level - base;
 	let percentage_multiplier = 1.0 / (4.0 * quarter);
 	let percentage = scaled_level * percentage_multiplier;
-	percentage.min(1.0).max(0.0)
+	percentage
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_risky_level() {
+		let ranges = [
+			HeightRange{normal_level: 1.3, risky_level: 4.6},
+			HeightRange{normal_level: 0.0, risky_level: 103.5},
+			HeightRange{normal_level: 12.0, risky_level: 53.5},
+			HeightRange{normal_level: 0.0, risky_level: 0.1}
+		];
+
+		for range in ranges {
+			let result = get_flood_percentage(range.risky_level, range);
+			assert_eq!(result, 0.5);
+		};
+	}
+
+	#[test]
+	fn test_normal_level() {
+		let ranges = [
+			HeightRange{normal_level: 1.3, risky_level: 4.6},
+			HeightRange{normal_level: 0.0, risky_level: 103.5},
+			HeightRange{normal_level: 12.0, risky_level: 53.5},
+			HeightRange{normal_level: 0.0, risky_level: 0.1}
+		];
+
+		for range in ranges {
+			let result = get_flood_percentage(range.normal_level, range);
+			assert_eq!(result, 0.25);
+		};
+	}
+
+	#[test]
+	fn test_values_above_risky_level() {
+		assert_eq!(get_flood_percentage(
+			3.0, 
+			HeightRange{normal_level: 1.0, risky_level: 2.0}), 
+			0.75
+		);
+		assert_eq!(get_flood_percentage(
+			26.0, 
+			HeightRange{normal_level: 20.0, risky_level: 25.0}), 
+			0.55
+		);
+		assert_eq!(get_flood_percentage(
+			30.0, 
+			HeightRange{normal_level: 10.0, risky_level: 15.0}), 
+			1.25
+		);
+	}
+
+	#[test]
+	fn test_values_below_normal_level() {
+		assert_eq!(get_flood_percentage(
+			0.0, 
+			HeightRange{normal_level: 1.0, risky_level: 2.0}), 
+			0.0
+		);
+		assert_eq!(get_flood_percentage(
+			19.0, 
+			HeightRange{normal_level: 20.0, risky_level: 25.0}), 
+			0.2
+		);
+		assert_eq!(get_flood_percentage(
+			0.0, 
+			HeightRange{normal_level: 10.0, risky_level: 15.0}), 
+			-0.25
+		);
+	}
 }
