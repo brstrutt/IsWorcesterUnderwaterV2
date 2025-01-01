7f465c63aaea88606f015d973f4d10e908b06fd5
brstrutt <ben.strutt@hotmail.co.uk>
2025-01-01 15:04:37 +0000

    add link to github

--- src/footer.rs
+++ src/footer.rs
@@ -5,6 +5,11 @@ pub fn footer() -> Html {
     html! {
         <footer>
             <div>{ "This uses Environment Agency flood and river level data from the real-time data API (Beta)" }</div>
+            <div>
+                <a href="https://github.com/brstrutt/IsWorcesterUnderwaterV2" target="_blank" rel="noopener noreferrer">
+                    <img src="/assets/GitHub-Mark-Light-64px.png" title="Github" alt="Github Logo" width="12px" height="12px"/>
+                </a>
+            </div>
             <div class="float_right">
                 <div>
                     { "Icons made by "}
